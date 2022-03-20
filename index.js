8de43baa7b3ed157b61a0ef861911c63db68ce8f
vikstasenkova <viktoria_stasenkova@mail.ru>
2022-03-20 21:52:35 +0300

    fix: code & design of my projects

---
+++ index.js
@@ -0,0 +1,12 @@
+const projectsText = document.querySelector(".projects__text");
+const projectsColumn = document.querySelector(".projects__column");
+const projectsImg = document.querySelector(".projects__img");
+
+// projectsImg.onmouseover = function(event){
+//     let target = event.target;
+//     projectsText.classList.add("active");
+// }
+// projectsImg.onmouseout = function(event){
+//     let target = event.target;
+//     projectsText.classList.remove("active");
+// }
